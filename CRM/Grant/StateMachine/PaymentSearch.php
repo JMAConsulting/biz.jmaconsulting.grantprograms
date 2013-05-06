19af562294f5233c5dc5854688f9a40e7dfade91
Pradeep Nayak <pradeep@pradeep.(none)>
2013-05-07 01:22:01 +0530

    -- fixed formatting for RG-108

--- CRM/Grant/StateMachine/PaymentSearch.php
+++ CRM/Grant/StateMachine/PaymentSearch.php
@@ -34,84 +34,77 @@
  *
  */
 
-require_once 'CRM/Core/StateMachine.php';
-require_once 'CRM/Core/Action.php';
-require_once 'CRM/Grant/PaymentTask.php';
+class CRM_Grant_StateMachine_PaymentSearch extends CRM_Core_StateMachine {
+  /**
+   * The task that the wizard is currently processing
+   *
+   * @var string
+   * @protected
+   */
+  protected $_task;
 
-class CRM_Grant_StateMachine_PaymentSearch extends CRM_Core_StateMachine
-{
-    /**
-     * The task that the wizard is currently processing
-     *
-     * @var string
-     * @protected
-     */
-    protected $_task;
-
-    /**
-     * class constructor
-     */
-    function __construct( $controller, $action = CRM_Core_Action::NONE )
-    {
-        parent::__construct( $controller, $action );
+  /**
+   * class constructor
+   */
+  function __construct($controller, $action = CRM_Core_Action::NONE) {
+    parent::__construct($controller, $action);
         
-        $this->_pages = array( );
+    $this->_pages = array();
         
-        $this->_pages['CRM_Grant_Form_PaymentSearch'] = null;
-        list( $task, $result ) = $this->taskName( $controller, 'PaymentSearch' );
-        $this->_task    = $task;
+    $this->_pages['CRM_Grant_Form_PaymentSearch'] = NULL;
+    list($task, $result) = $this->taskName($controller, 'PaymentSearch');
+    $this->_task = $task;
         
-        if ( is_array( $task ) ) {
-            foreach ( $task as $t ) {
-                $this->_pages[$t] = null;
-            }
-        } else {
-            $this->_pages[$task] = null;
-        }
-
-        if ( $result ) {
-            $this->_pages['CRM_Grant_Form_Task_Result'] = null;
-        }
-        
-        $this->addSequentialPages( $this->_pages, $action );
+    if (is_array($task)) {
+      foreach ($task as $t) {
+        $this->_pages[$t] = NULL;
+      }
+    } 
+    else {
+      $this->_pages[$task] = NULL;
     }
+
+    if ($result) {
+      $this->_pages['CRM_Grant_Form_Task_Result'] = NULL;
+    }
+    $this->addSequentialPages($this->_pages, $action);
+  }
     
-    /**
-     * Determine the form name based on the action. This allows us
-     * to avoid using  conditional state machine, much more efficient
-     * and simpler
-     *
-     * @param CRM_Core_Controller $controller the controller object
-     *
-     * @return string the name of the form that will handle the task
-     * @access protected
-     */
-    function taskName( $controller, $formName = 'PaymentSearch' )
-    {
-        // total hack, check POST vars and then session to determine stuff
-        // fix value if print button is pressed
-        if ( CRM_Utils_Array::value( '_qf_' . $formName . '_next_print', $_POST ) ) {
-            $value = CRM_Grant_Task::PRINT_GRANTS;
-        } else {
-            $value = CRM_Utils_Array::value( 'task', $_POST );
-        }
-        if ( ! isset( $value ) ) {
-            $value = $this->_controller->get( 'task' );
-        }
-        $this->_controller->set( 'task', $value );
+  /**
+   * Determine the form name based on the action. This allows us
+   * to avoid using  conditional state machine, much more efficient
+   * and simpler
+   *
+   * @param CRM_Core_Controller $controller the controller object
+   *
+   * @return string the name of the form that will handle the task
+   * @access protected
+   */
+  function taskName($controller, $formName = 'PaymentSearch') {
+    // total hack, check POST vars and then session to determine stuff
+    // fix value if print button is pressed
+    if (CRM_Utils_Array::value( '_qf_' . $formName . '_next_print', $_POST)) {
+      $value = CRM_Grant_Task::PRINT_GRANTS;
+    } 
+    else {
+      $value = CRM_Utils_Array::value('task', $_POST);
+    }
+    if (!isset($value)) {
+      $value = $this->_controller->get('task');
+    }
+    $this->_controller->set('task', $value);
         
-        return CRM_Grant_PaymentTask::getTask( $value );  
-    }
+    return CRM_Grant_PaymentTask::getTask($value);  
+  }
     
-    /**
-     * return the form name of the task
-     *
-     * @return string
-     * @access public
-     */
-    function getTaskFormName( )
-    {
-        return CRM_Utils_String::getClassName( $this->_task );
-    }
+  /**
+   * return the form name of the task
+   *
+   * @return string
+   * @access public
+   */
+  function getTaskFormName() {
+    return CRM_Utils_String::getClassName($this->_task);
+  }
 }
 
