bb5681459e0296c455f638e5de8c76ff37b6f73e
Mayur Jadhav <mayur@mayur-desktop>
2013-06-03 14:34:18 +0530

    Worked on RG-163

--- CRM/Grant/PaymentTask.php
+++ CRM/Grant/PaymentTask.php
@@ -2,7 +2,7 @@
 
 /*
  +--------------------------------------------------------------------+
- | CiviCRM version 4.1                                                |
+ | CiviCRM version 4.3                                                |
  +--------------------------------------------------------------------+
  | Copyright CiviCRM LLC (c) 2004-2011                                |
  +--------------------------------------------------------------------+
@@ -39,116 +39,116 @@
  * used by the search forms
  *
  */
-class CRM_Grant_PaymentTask
-{
-    const
-      CANCEL_PAYMENTS                     =     1,
-      REPRINT_PAYMENTS                    =     2;
-     
-    /**
-     * the task array
-     *
-     * @var array
-     * @static
-     */
-    static $_tasks = null;
-
-    /**
-     * the optional task array
-     *
-     * @var array
-     * @static
-     */
-    static $_optionalTasks = null;
-
-    /**
-     * These tasks are the core set of tasks that the user can perform
-     * on a contact / group of contacts
-     *
-     * @return array the set of tasks for a group of contacts
-     * @static
-     * @access public
-     */
-    static function &tasks( )
-    {
-        if ( !( self::$_tasks ) ) {
-            self::$_tasks = array( 1 => array( 'title'  => ts( 'Cancel Payments' ),
-                                               'class'  => 'CRM_Grant_Form_Task_Cancel',
-                                               'result' => false ),
-                                   2 => array( 'title'  => ts( 'Reprint Payments' ),
-                                               'class'  => 'CRM_Grant_Form_Task_Reprint',
-                                               'result' => false ),
-                                   
-                                   );
-        }
-        if ( !CRM_Core_Permission::check( 'cancel payments in CiviGrant' ) ) {
-            unset( self::$_tasks[1] );
-        }
-        
-        if ( !CRM_Core_Permission::check( 'edit payments in CiviGrant' ) && !CRM_Core_Permission::check( 'create payments in CiviGrant' )) {
-            unset( self::$_tasks[2] );
-        }
-        
-        require_once 'CRM/Utils/Hook.php';
-        CRM_Utils_Hook::searchTasks( 'grant', self::$_tasks );
-        asort( self::$_tasks );
-        return self::$_tasks;
+class CRM_Grant_PaymentTask {
+  const
+    CANCEL_PAYMENTS = 1,
+    REPRINT_PAYMENTS = 2;
+  
+  /**
+   * the task array
+   *
+   * @var array
+   * @static
+   */
+  static $_tasks = NULL;
+  
+  /**
+   * the optional task array
+   *
+   * @var array
+   * @static
+   */
+  static $_optionalTasks = NULL;
+  
+  /**
+   * These tasks are the core set of tasks that the user can perform
+   * on a contact / group of contacts
+   *
+   * @return array the set of tasks for a group of contacts
+   * @static
+   * @access public
+   */
+  static function &tasks() {
+    if (!(self::$_tasks)) {
+      self::$_tasks = array(
+        1 => array(
+          'title' => ts('Cancel Payments'),
+          'class' => 'CRM_Grant_Form_Task_Cancel',
+          'result' => FALSE
+        ),
+        2 => array(
+          'title' => ts('Reprint Payments'),
+          'class' => 'CRM_Grant_Form_Task_Reprint',
+          'result' => false
+        ),
+      );
+    }
+    if (!CRM_Core_Permission::check('cancel payments in CiviGrant')) {
+      unset(self::$_tasks[1]);
     }
     
-    /**
-     * These tasks are the core set of task titles
-     *
-     * @return array the set of task titles 
-     * @static
-     * @access public
-     */
-    static function &taskTitles( )
-    {
-        self::tasks( );
-        $titles = array( );
-        foreach ( self::$_tasks as $id => $value ) {
-          $titles[$id] = $value['title'];
-        }      
-        return $titles;
+    if (!CRM_Core_Permission::check('edit payments in CiviGrant') 
+      && !CRM_Core_Permission::check('create payments in CiviGrant')) {
+      unset(self::$_tasks[2]);
     }
     
-    /**
-     * show tasks selectively based on the permission level
-     * of the user
-     *
-     * @param int $permission
-     *
-     * @return array set of tasks that are valid for the user
-     * @access public
-     */
-    static function &permissionedTaskTitles( $permission ) 
-    {
-        $tasks = array( );
-        if ( ( $permission == CRM_Core_Permission::EDIT ) 
-             || CRM_Core_Permission::check( 'edit payments in CiviGrant' ) ) {
-            $tasks = self::taskTitles( );
-        } else {
-            $tasks = array(
-                           3 => self::$_tasks[3]['title'] );
-            //CRM-4418,
-            if ( CRM_Core_Permission::check( 'cancel payments in CiviGrant' ) ) {
-                $tasks[1] = self::$_tasks[1]['title']; 
-            }
-        }
-        return $tasks;
+    require_once 'CRM/Utils/Hook.php';
+    CRM_Utils_Hook::searchTasks('grant', self::$_tasks);
+    asort(self::$_tasks);
+    return self::$_tasks;
+  }
+  
+  /**
+   * These tasks are the core set of task titles
+   *
+   * @return array the set of task titles 
+   * @static
+   * @access public
+   */
+  static function &taskTitles() 
+    self::tasks( );
+    $titles = array( );
+    foreach (self::$_tasks as $id => $value) {
+      $titles[$id] = $value['title'];
+    }      
+    return $titles;
+  }
+  
+  /**
+   * show tasks selectively based on the permission level
+   * of the user
+   *
+   * @param int $permission
+   *
+   * @return array set of tasks that are valid for the user
+   * @access public
+   */
+  static function &permissionedTaskTitles($permission) {
+    $tasks = array( );
+    if (($permission == CRM_Core_Permission::EDIT) 
+      || CRM_Core_Permission::check('edit payments in CiviGrant')) {
+      $tasks = self::taskTitles();
+    } else {
+      $tasks = array(
+        3 => self::$_tasks[3]['title']);
+      //CRM-4418,
+      if (CRM_Core_Permission::check('cancel payments in CiviGrant')) {
+        $tasks[1] = self::$_tasks[1]['title']; 
+      }
     }
+    return $tasks;
+  }
     
-    /**
-     * These tasks are the core set of tasks that the user can perform
-     *
-     * @param int $value
-     *
-     * @return array the set of tasks for a group of contacts
-     * @static
-     * @access public
-     */
-    static function getTask( $value ) 
-    {
-    }
+  /**
+   * These tasks are the core set of tasks that the user can perform
+   *
+   * @param int $value
+   *
+   * @return array the set of tasks for a group of contacts
+   * @static
+   * @access public
+   */
+  static function getTask($value) {
+  }
 }
 
