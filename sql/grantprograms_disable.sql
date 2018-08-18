8bc0d80b71cb1d34a7752a88210d1905c45653ef
deb.monish <deb.monish@gmail.com>
2018-08-18 15:00:47 +0530

    minor changes

--- sql/grantprograms_disable.sql
+++ sql/grantprograms_disable.sql
@@ -26,11 +26,11 @@
  *          Canada   M5T 2C7
  */
 
-UPDATE civicrm_option_group SET is_active = 0 WHERE name IN ('grant_payment_status','grant_program_status', 'allocation_algorithm', 'grant_thresholds', 'reason_grant_ineligible', 'reason_grant_incomplete', 'grant_info_too_late', 'msg_tpl_workflow_grant'):
+UPDATE civicrm_option_group SET is_active = 0 WHERE name IN ('grant_payment_status','grant_program_status', 'allocation_algorithm', 'grant_thresholds', 'reason_grant_ineligible', 'reason_grant_incomplete', 'grant_info_too_late', 'msg_tpl_workflow_grant');
 
 UPDATE civicrm_option_value SET is_active = 0 WHERE option_group_id IN (SELECT id FROM civicrm_option_group WHERE name IN ('grant_payment_status','grant_program_status', 'allocation_algorithm', 'grant_thresholds', 'reason_grant_ineligible', 'reason_grant_incomplete', 'grant_info_too_late', 'msg_tpl_workflow_grant'));
 
 -- RG-212
-UPDATE  civicrm_option_group cog INNER JOIN civicrm_option_value cov ON cov.option_group_id = cog.id 
+UPDATE  civicrm_option_group cog INNER JOIN civicrm_option_value cov ON cov.option_group_id = cog.id
 SET cov.is_active = 0
 WHERE  cog.name = 'activity_type' AND cov.name IN ('grant_status_change', 'grant_payment');
