c913cf9f66b89795d092ff4686b55703fc2b8497
Monish Deb <deb.monish@gmail.com>
2018-09-24 21:03:06 +0530

    minor fixes

--- sql/grantprograms_install.sql
+++ sql/grantprograms_install.sql
@@ -1,31 +1,3 @@
-/**
- * Grant Programs extension improves grant allocation
- * in CiviGrant
- *
- * Copyright (C) 2012 JMA Consulting
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Support: https://github.com/JMAConsulting/biz.jmaconsulting.grantprograms/issues
- *
- * Contact: info@jmaconsulting.biz
- *          JMA Consulting
- *          215 Spadina Ave, Ste 400
- *          Toronto, ON
- *          Canada   M5T 2C7
- */
-
 -- create civicrm_payment table.
 CREATE TABLE IF NOT EXISTS `civicrm_payment` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Id',
@@ -34,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `civicrm_payment` (
   `payment_reason` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Payment Reason.',
   `payment_status_id` int(10) unsigned DEFAULT NULL COMMENT 'Payment Status ID',
   `replaces_payment_id` varchar(8) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Replaces Payment Id.',
-  `replaces_check_number` varchar(20) unsigned NOT NULL COMMENT 'Replaced Check Number',
+  `replaces_check_number` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Replaced Check Number',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
 
